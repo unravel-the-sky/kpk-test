4309ac591bfa8775e0c78fc3e89987eb15702e26
unravel-the-sky <sekdemir@gmail.com>
2017-07-16 21:06:50 +0200

    update; trying to get php thingy work;

---
+++ portal.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        Wellcome! <?php echo $_GET["name"]; ?> <br>
+        You are registered to the following: <br>
+        <?php echo $_GET["info"]; ?>         
+    </body>
+</html>
