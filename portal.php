b8c896897786328899dc1c8b310f2a5f609b3726
unravel-the-sky <sekdemir@gmail.com>
2017-07-16 21:33:42 +0200

    update; php works, sending mail as well, yay;

--- portal.php
+++ portal.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
 <html>
     <body>
-        Wellcome! <?php echo $_GET["name"]; ?> <br>
+        Wellcome <?php echo $_POST["navn"]; ?> ! <br>
         You are registered to the following: <br>
-        <?php echo $_GET["info"]; ?>         
+        <?php echo $_POST["info"]; ?> <br>
+
+        Email is just being sent now! <br>
+        <?PHP
+            $email = "sekdemir@gmail.com";
+            $to = "sekdemir@gmail.com";
+            $subject = "New Email Address for Mailing List";
+            $headers = "From: $email\n";
+            $message = "A visitor to your site has sent the following email address to be added to your mailing list.\n 
+            Email Address: $email";
+            
+            $user = "$email";
+            $usersubject = "Thank You";
+            $userheaders = "From: you@youremailaddress.com\n";
+            $usermessage = $_POST["navn"];
+            mail($to,$subject,$message,$headers);
+            mail($user,$usersubject,$usermessage,$userheaders);
+        ?>        
     </body>
 </html>
